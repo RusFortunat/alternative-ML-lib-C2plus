846fb5ac634f19c1fc6be83cbc1b7e6db2505c2c
RusFortunat <mruslani@vt.edu>
2023-09-01 15:51:22 +0200

    the classifier draft is finished, need to debug now

---
+++ image_classification/check_data_set.py
@@ -0,0 +1,119 @@
+# I followed this PyTorch tutorial to write the code: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+# Change 'torchvision.datasets.CIFAR10' to whatever dataset you want to work with.
+# The list of datasets available for the training can be found here: https://pytorch.org/vision/0.8/datasets.html#mnist
+# Take care of whether the images you load are gray-scale or colored, that changes the number of input channels
+
+import time
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class FCNet(nn.Module):
+    def __init__(self, image_shape, hidden_layer_FC1, hidden_layer_FC2):
+        super().__init__()
+        self.fc1 = nn.Linear(image_shape, hidden_layer_FC1) # DIFFERENT FOR DIFFERENT DATA SETS; I'm lazy & stupid to automize it on the go
+        self.fc2 = nn.Linear(hidden_layer_FC1, hidden_layer_FC2)
+        self.fc3 = nn.Linear(hidden_layer_FC2, 10)
+
+    def forward(self, x):
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+# Have to wrap the main to run the code on Windows, sorry not sorry
+def main():
+
+    batch_size = 4
+
+    # for grayscale images
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
+
+    # for colored images
+    #transform = transforms.Compose(
+    #    [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    
+    # download images from CIFAR10 datasel
+    trainset = torchvision.datasets.KMNIST(root='D:/Work/data_sets/cifar-10-batches-py', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.KMNIST(root='D:/Work/data_sets/cifar-10-batches-py', train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+    
+    # just to get image dimensions for FC network
+    dataiter = iter(trainloader)
+    images, labels = next(dataiter)
+    image_dims = images[0].shape[0] * images[0].shape[1] * images[0].shape[2] # here I am assuming that the image has [NChannels x H x W] .shape format
+
+    print(f'Image dimensions: {images[0].shape[0]} x {images[0].shape[1]} x {images[0].shape[2]}')
+    print(f'Flattened vector size: {image_dims}')
+
+    # initialize instances of FC and ConvNet classes
+    in_channels = 1 # important for CNN networks: 1 for grayscale, 3 for colored
+    hidden_layer_FC1 = 200 # 120 tutorial value
+    hidden_layer_FC2 = 100 # 84 tutorial value
+    fcNet = FCNet(image_dims, hidden_layer_FC1, hidden_layer_FC2)
+    num_of_FC_params = count_parameters(fcNet)
+
+    criterion = nn.CrossEntropyLoss()
+    
+    # train FC model
+    start_time = time.time()
+    optimizerFC = optim.SGD(fcNet.parameters(), lr=0.001, momentum=0.9)
+    for epoch in range(5):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizerFC.zero_grad()
+
+            # forward + backward + optimize
+            outputs = fcNet(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizerFC.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] FC loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+    FC_training_time = time.time() - start_time
+    print('Finished Training FC model')
+
+
+    # run the trained model through the whole dataset
+    correctFC = 0
+    total = 0
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            # calculate outputs by running images through the network
+            outputFC = fcNet(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predictedFC = torch.max(outputFC.data, 1)
+            total += labels.size(0)
+            correctFC += (predictedFC == labels).sum().item()
+
+    print(f'FC training time: {FC_training_time} sec')
+    print(f'Number of FC model parameters: {num_of_FC_params}')
+    print(f'Accuracy of the FC network on the 10000 test images: {100 * correctFC // total} %')
+
+
+if __name__ == '__main__':
+    main()
--- image_classification/image_classification.cpp
+++ image_classification/image_classification.cpp
@@ -1,5 +1,6 @@
 // Here I will use a fully-connected network with 1 hidden layer to classify images. 
 // I will test my code on MNIST database of handwritten number and letters.
+// Download the MNIST handwritten images dataset from here: http://www.pymvpa.org/datadb/mnist.html
 
 #include <iostream>
 #include <vector>
@@ -13,29 +14,95 @@
 
 using namespace std;
 
+// load the dataset and the labels; I copied these functions from here: https://stackoverflow.com/a/33384846 
+unsigned char** read_mnist_images(string full_path, int& number_of_images, int& image_size) {
+    auto reverseInt = [](int i) {
+        unsigned char c1, c2, c3, c4;
+        c1 = i & 255, c2 = (i >> 8) & 255, c3 = (i >> 16) & 255, c4 = (i >> 24) & 255;
+        return ((int)c1 << 24) + ((int)c2 << 16) + ((int)c3 << 8) + c4;
+    };
+
+    typedef unsigned char uchar;
+
+    ifstream file(full_path, ios::binary);
+
+    if (file.is_open()) {
+        int magic_number = 0, n_rows = 0, n_cols = 0;
+
+        file.read((char*)&magic_number, sizeof(magic_number));
+        magic_number = reverseInt(magic_number);
+
+        if (magic_number != 2051) throw runtime_error("Invalid MNIST image file!");
+
+        file.read((char*)&number_of_images, sizeof(number_of_images)), number_of_images = reverseInt(number_of_images);
+        file.read((char*)&n_rows, sizeof(n_rows)), n_rows = reverseInt(n_rows);
+        file.read((char*)&n_cols, sizeof(n_cols)), n_cols = reverseInt(n_cols);
+
+        image_size = n_rows * n_cols;
+
+        uchar** _dataset = new uchar * [number_of_images];
+        for (int i = 0; i < number_of_images; i++) {
+            _dataset[i] = new uchar[image_size];
+            file.read((char*)_dataset[i], image_size);
+        }
+        return _dataset;
+    }
+    else {
+        throw runtime_error("Cannot open file `" + full_path + "`!");
+    }
+}
+
+unsigned char* read_mnist_labels(string full_path, int& number_of_labels) {
+    auto reverseInt = [](int i) {
+        unsigned char c1, c2, c3, c4;
+        c1 = i & 255, c2 = (i >> 8) & 255, c3 = (i >> 16) & 255, c4 = (i >> 24) & 255;
+        return ((int)c1 << 24) + ((int)c2 << 16) + ((int)c3 << 8) + c4;
+    };
+
+    typedef unsigned char uchar;
+
+    ifstream file(full_path, ios::binary);
+
+    if (file.is_open()) {
+        int magic_number = 0;
+        file.read((char*)&magic_number, sizeof(magic_number));
+        magic_number = reverseInt(magic_number);
+
+        if (magic_number != 2049) throw runtime_error("Invalid MNIST label file!");
+
+        file.read((char*)&number_of_labels, sizeof(number_of_labels)), number_of_labels = reverseInt(number_of_labels);
+
+        uchar* _dataset = new uchar[number_of_labels];
+        for (int i = 0; i < number_of_labels; i++) {
+            file.read((char*)&_dataset[i], 1);
+        }
+        return _dataset;
+    }
+    else {
+        throw runtime_error("Unable to open file `" + full_path + "`!");
+    }
+}
+
 int main(){
 
     // NN parameters
-    int input_size = 10;
-    int hidden_size = 10;
-    int output_size = 10;
+    int input_size = 28 * 28; // image size
+    int hidden_size = 128;
+    int output_size = 10; // number of labels
     double learning_rate = 0.001;  
-    
-    int data_set_size = 200;
+    int data_set_size = 200; // number of images
     int batch_size = 20;
-    int training_episodes = 10;
+    int training_episodes = 5;
+
+    // load images
+    string train_path_images = "";
+    string train_path_labels = "";
+    int data_set_size = 100;
+    unsigned char** train_dataset = read_mnist_images(train_path_images, data_set_size, input_size); // input_size = image size
+    unsigned char* train_labels = read_mnist_labels(train_path_labels, output_size); // output_size = numer of labels
 
     // define out network
     Tensor model(input_size, hidden_size, output_size, learning_rate);
-
-    // load images; the MNIST data set is huge (~1GB in size), so you have to download it yourself 
-    // and specify your path to the folder with the data
-    
-
-
-
-
-
 
     // train the network
     printf("Train the network on a randomly generated data set:\n");
@@ -60,8 +127,10 @@ int main(){
             // create a mini-batch
             for (auto i = 0; i < batch_size; i++) {
                 int index = element_index[minibatch * batch_size + i];
-                vector<double> input = x[index];
-                vector<double> target = y[index];
+                vector<double> input = train_dataset[index];
+                vector<double> target(output_size);
+                int true_label = train_labels[index];
+                target[true_label] = 1; // prepare the target vector in a way that only one element is non-zero
 
                 vector<double> predicted = model.forward(input); // model prediction
                 model.compute_gradients(input, predicted, target, 
@@ -77,23 +146,28 @@ int main(){
         }
         printf("Episode = %d; Loss = %.2f \n ", episode, loss);
     }
+    printf("The training is done. Time to see how well the network can classify the images..\n");
 
-    // save data and parameters
-    ofstream output_data("./output/random_data.txt");
-    for(auto i = 0; i < data_set_size; i++){
-        output_data << x[i][0] << "\t" << y[i][0] << "\n";
+    // test your network
+    string test_path_images = "";
+    string test_path_labels = "";
+    int test_set_size = 100;
+    unsigned char** test_dataset = read_mnist_images(test_path_images, test_set_size, input_size); // input_size = image size
+    unsigned char* test_labels = read_mnist_labels(test_path_labels, output_size); // output_size = numer of labels
+
+    int correct = 0;
+    for (auto i = 0; i < test_set_size; i++) {
+        vector<double> input = test_dataset[i];
+        vector<double> target(output_size);
+        int true_label = test_labels[i];
+        target[true_label] = 1; // prepare the target vector in a way that only one element is non-zero
+
+        vector<double> predicted = model.forward(input); // model prediction
+        int max_label = *max_element(begin(predicted), end(predicted));
+        if (max_label == true_label) correct++;
     }
-    
-    ofstream output_parameters("./output/random_data_network_parameters.txt");
-    tuple<vector<vector<double>>, vector<double>, 
-        vector<vector<double>>, vector<double>> weights_and_biases = model.model_parameters();
-    vector<vector<double>> weights1 = get<0>(weights_and_biases);
-    vector<vector<double>> weights2 = get<2>(weights_and_biases);
-    vector<double> biases1 = get<1>(weights_and_biases);
-    vector<double> biases2 = get<3>(weights_and_biases);
-    // i will be very crude here and use the knowledge that we only have 1 weight and 1 bias for the linear regression task
-    output_parameters << "weights1 " << weights1[0][0] << "; biases1 " << biases1[0]
-        << "; weights2 " << weights2[0][0] << "; biases2 " << biases2[0]; 
+    printf("Number of correctly predicted images: %f\n", 1.0 * correct / (1.0 * test_set_size));
+
 
     return 0;
 }
