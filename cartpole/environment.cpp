fe7c9e71e297c7abfabb96de759c9b61bb88c90f
Ruslan Mukhamadiarov <Ruslan.Mukhamadiarov@th-ea-lfyusb04.mobil.theorie.physik.uni-muenchen.de>
2023-09-06 16:00:58 +0200

    structure for Environment files is outlied

--- cartpole/environment.cpp
+++ cartpole/environment.cpp
@@ -4,12 +4,32 @@
 #include <cmath>
 #include <vector>
 #include <ctime>
+#include "Environment.h"
 
 using namespace std;
+
+Environment::Environment(double M, double m, double L){
+
+    _M = M; // mass of the block that can be pushed
+    _m = m; // mass of the rod
+    _L = L; // length of the rod
+    // i will be always starting with x=0, x_dot = 0, and \theta = 0, \theta_dot = 0
+    _x = 0; // cart position
+    _x_dot = 0; // cart velocity
+    _theta = 0; // the angle that the rod makes from the vertical axis
+    _theta_dot = 0; // rod's angular velocity
+
+}
+
+Environment::~Environment(){
+    // i'll just put it here
+}
+
 
 // the function receives the action to push cartpole to the left or to the right
 vector<double> Environment::update(int action){
 
+    // 
 
 
 
@@ -17,7 +37,6 @@ vector<double> Environment::update(int action){
 
 
 
-
-
-
+    // return x, x_dot, theta, and theta_dot 
+    return _x, _x_dot, _theta, _theta_dot;
 }
--- cartpole/environment.h
+++ cartpole/environment.h
@@ -1,44 +1,25 @@
-// File with Tensor class constructor, destructor, and methods
+// File with Environment class methods and variables
 // Authors: 
 #pragma once
 #include <iostream>
 #include <vector>
 using namespace std;
 
-class Tensor{
+class Environment{
     public:
-        // for now all tensors will have just a single hidden layer
-        Tensor(int input_size, int hidden_size, 
-        int output_size, double learning_rate); // constructor with random parameters
-        
-        ~Tensor();
+        double _x;
+        double _x_dot;
+        double _theta;
+        double _theta_dot;
 
-        vector<double> forward(vector<double> &input_vector); // forwardprop with ReLU activation
-        
-        // compute gradients for backprop
-        void compute_gradients(vector<double>& input_vector, 
-            vector<double>& predicted_vector, 
-            vector<double>& target_vector,
-            vector<vector<double>>& w_gradients1, vector<double>& b_gradients1, 
-            vector<vector<double>>& w_gradients2, vector<double>& b_gradients2, int batch_size); 
+        // not sure if anything else is needed 
+        Environment(double M, double m, double L); // constructor with random parameters
+        ~Environment();
 
-        // update network parameters
-        void optimizer_step(vector<vector<double>>& w_gradients1, vector<double>& b_gradients1,
-            vector<vector<double>>& w_gradients2, vector<double>& b_gradients2); 
-        
-        // allows accessing network parameters
-        tuple<vector<vector<double>>, vector<double>, 
-            vector<vector<double>>, vector<double>> model_parameters();
+        vector<double> update(int action); // forwardprop with ReLU activation
 
-    private: 
-        int _input_size;
-        int _hidden_size;
-        int _output_size;
-        double _learning_rate;
-        vector<vector<double>> _W1;
-        vector<vector<double>> _W2;
-        vector<double> _B1;
-        vector<double> _B2;
-
-        vector<double> _hidden_vector;
+    private:
+        double _M;
+        double _m;
+        double _L;
 };
