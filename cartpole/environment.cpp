428ee96e347995e9333608c129df711a4349bf32
RusFortunat <mruslani@vt.edu>
2023-09-07 18:11:05 +0200

    environment is ready

--- cartpole/environment.cpp
+++ cartpole/environment.cpp
@@ -13,33 +13,38 @@ Environment::Environment(double M, double m, double L){
     _M = M; // mass of the block that can be pushed
     _m = m; // mass of the rod
     _L = L; // length of the rod
+
     // i will be always starting with x=0, x_dot = 0, and \theta = 0, \theta_dot = 0
     _x = 0; // cart position
     _x_dot = 0; // cart velocity
     _theta = 0; // the angle that the rod makes from the vertical axis
     _theta_dot = 0; // rod's angular velocity
-
 }
 
 Environment::~Environment(){
     // i'll just put it here
 }
 
-
 // the function receives the action to push cartpole to the left or to the right
 vector<double> Environment::update(int action){
 
-    // what do we modify with the action? I am not going to look what others did, and just increment the velocity
-    double velocity_increment = 0.01;
-    if(action == 0){ // push the cart to the left
-        velocity_increment *= -1.0;
-    }
-    _x_dot += velocity_increment;
+    // Will copy this implementation from R. Sutton's book: http://incompleteideas.net/sutton/book/code/pole.c 
 
-    // solve coupled EOMs; just a block, and a rod, attached from one end to it; no friction
+    double Force = action == 0 ? -10.0 : 10.0; // push the cart to the left for action = 0; otherwise to the right
+    double TAU = 0.02; // time increment
+    double g = 9.0; // gravity
+    double costheta = cos(_theta);
+    double sintheta = sin(_theta);
 
+    double temp = (Force + _L * _theta_dot * _theta_dot * sintheta) / (_M + _m);
+    double thetaacc = (g * sintheta - costheta * temp) / (_L * (4.0/3.0 - _m * costheta * costheta / (_M + _m)));
+    double xacc = temp - _m * _L * thetaacc * costheta / (_M + _m);
 
-
+    /*** Update the four state variables, using Euler's method. ***/
+    _x += TAU * _x_dot;
+    _x_dot += TAU * xacc;
+    _theta += TAU * _theta_dot;
+    _theta_dot += TAU * thetaacc;
 
     // return x, x_dot, theta, and theta_dot 
     return _x, _x_dot, _theta, _theta_dot;
